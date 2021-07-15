efc1a0552fab57d15d14bd290262a9b08932569c
Leontii Mikshanschi <leontii.mik@gmail.com>
2021-07-15 17:57:38 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const gretStr = 'hello';
+
+function sqrt () {
+  let result = 'pass';
+  return result;
+  // console.log(gretStr);
+  // return result;
+}
+
+
+
+// const divOut = document.querySelector('#temp');
+// let res = sqrt();
+// divOut.innerHTML = res;
+// console.log(gretStr);
+
+// window.addEventListener('DOMContentLoaded', function () {
+//   let pr = document.querySelectorAll('p');
+//   console.log(pr);
+//   for(let i = 0; i < pr.length; i++)
+//   pr[i].style.color = 'cyan';
+// })
+
+// let x = `<div class="">${}</div>`;
+
+let sum = (x, y) => {
+  let result = x + y;
+  return result;
+}
+
